b51fa490ada0ba6792b0cd4e175bc1b5cc8c64c6
lmarti-dev <lucas.marti@dxdt.ch>
2024-03-05 09:03:45 +0100

    style change

--- tests/test_plots_styles.py
+++ tests/test_plots_styles.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-from fauplotstyle.styler import use_style, get_fau_cmap
+from fauplotstyle.styler import use_style
 
 
-def test_default_plot():
+def test_default_plot4():
     use_style()
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(ncols=2, nrows=2)
     axes = (ax1, ax2, ax3, ax4)
@@ -23,16 +23,36 @@ def test_default_plot():
     plt.show()
 
 
-def test_cmap_plot(cmap):
+def test_default_plot2():
+    use_style()
+    fig, (ax1, ax2) = plt.subplots(nrows=2)
+    axes = (ax1, ax2)
+    for ax in axes:
+        for uu in range(np.random.randint(1, 7)):
+            x = sorted(np.random.rand(100))
+            y = sorted(np.random.rand(100))
+            ax.plot(x, y, label=uu)
+        ax.legend()
+        ax.set_xlabel("X-label")
+        ax.set_ylabel("Y-label")
+
+    fig.suptitle("Suptitle")
+    fig.tight_layout()
+    plt.show()
+
+
+def test_cmap_plot():
     """
     Helper function to plot data with associated colormap.
     """
     np.random.seed(19680801)
     data = np.random.randn(30, 30)
     fig, ax = plt.subplots()
+    cmap = plt.get_cmap("faucmap")
     psm = ax.pcolormesh(data, cmap=cmap, rasterized=True, vmin=-4, vmax=4)
     fig.colorbar(psm, ax=ax)
     plt.show()
 
 
-test_cmap_plot(cmap=get_fau_cmap())
+test_default_plot2()
+test_cmap_plot()
