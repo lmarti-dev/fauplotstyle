bce16a3aafda8a0f961bd2e5d6839c2c818acb2e
lmarti <lucas.marti@fau.de>
2026-02-19 10:37:24 +0100

    made it a package, thanks uv

--- src/fauplotstyle/tests/test_plots_styles.py
+++ src/fauplotstyle/tests/test_plots_styles.py
@@ -7,7 +7,9 @@ from fauplotstyle.styler import style
 
 def test_default_plot4():
     style()
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(ncols=2, nrows=2)
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(
+        ncols=2, nrows=2, constrained_layout=True
+    )
     axes = (ax1, ax2, ax3, ax4)
     for ax in axes:
         for uu in range(np.random.randint(1, 7)):
@@ -19,13 +21,14 @@ def test_default_plot4():
         ax.set_ylabel("Y-label")
 
     fig.suptitle("Suptitle")
-    fig.tight_layout()
+
     plt.show()
 
 
 def test_default_plot2():
     style()
-    fig, (ax1, ax2) = plt.subplots(nrows=2)
+
+    fig, (ax1, ax2) = plt.subplots(nrows=2, constrained_layout=True)
     axes = (ax1, ax2)
     for ax in axes:
         for uu in range(np.random.randint(1, 7)):
@@ -37,7 +40,6 @@ def test_default_plot2():
         ax.set_ylabel("Y-label")
 
     fig.suptitle("Suptitle")
-    fig.tight_layout()
     plt.show()
 
 
@@ -48,11 +50,23 @@ def test_cmap_plot():
     np.random.seed(19680801)
     data = np.random.randn(30, 30)
     fig, ax = plt.subplots()
-    cmap = plt.get_cmap("faucmap")
+    cmap = plt.get_cmap("fau_cmap")
     psm = ax.pcolormesh(data, cmap=cmap, rasterized=True, vmin=-4, vmax=4)
     fig.colorbar(psm, ax=ax)
     plt.show()
 
 
+def test_error_bar_plot():
+    np.random.seed(3132)
+    data = np.random.rand(100)
+    std = np.random.rand(100) / 10
+
+    fig, ax = plt.subplots()
+    ax.errorbar(range(len(data)), data, std)
+    plt.show()
+
+
 test_default_plot2()
+test_default_plot4()
+test_error_bar_plot()
 test_cmap_plot()
